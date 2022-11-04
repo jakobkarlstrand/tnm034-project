f189aba754bb66cdde22808fae6cb54bacf52519
Max Eriksson <maxer142@student.liu.se>
2022-11-04 12:56:57 +0100

    Eye detection should work now.

--- eye_detection.m
+++ eye_detection.m
@@ -2,19 +2,18 @@
 %%and edge-based. It then combines the returned images according to the 
 % "A hybrid method for eyesdetection in facial images" paper.
 
-function [illColImage, colEdgeImage, illEdgeImage, hybridImage1, hybridImage2] = eye_detection(currentImage)
+function [hybridImage] = eye_detection(currentImage)
 [chromaIm, lumaIm, illuImage] = eye_detection_illumination_based(currentImage);
 colourImage = eye_detection_colour_based(currentImage);
 edgeImage = eye_detection_edge_based(currentImage);
 
-%illuImage = eye_rules(illuImage);
-%colourImage = eye_rules(colourImage);
-%edgeImage = eye_rules(edgeImage);
+illuImage = eye_rules(illuImage);
+colourImage = eye_rules(colourImage);
+edgeImage = eye_rules(edgeImage);
 
 %%combinding images.
-illColImage = illuImage.*colourImage;
-colEdgeImage = colourImage.*edgeImage;
-illEdgeImage = illuImage.*edgeImage;
-hybridImage1 = illColImage.*illEdgeImage;
-hybridImage2 = illColImage.*colEdgeImage;
+illColImage = illuImage & colourImage;
+colEdgeImage = colourImage & edgeImage;
+illEdgeImage = illuImage & edgeImage;
+hybridImage = illColImage | colEdgeImage | illEdgeImage; %%Combine the images
 end
--- eye_rules.m
+++ eye_rules.m
@@ -7,27 +7,22 @@
 
 function [outImage] = eye_rules(inImage)
 Threshold = 0.5;
-regions = regionprops(inImage, 'Solidity', 'BoundingBox');
-
-% TODO
-% • The solidity of the region is greater than 0.5.
-% • The aspect ratio is between 0.8 and 4.0.
-% • The orientation of the connected component is between. 
-% -45 and +45 degree
+regions = regionprops(inImage, 'Solidity', 'BoundingBox', 'Orientation', 'PixelList');
+outImage = imclearborder(inImage); %%Remove objects touching the border.
 
 for k=1:length(regions)
-Solidity = regions(k).Solidity();
-bound = regions(k).BoundingBox();
-    
-bx = uint8(bound(1)); % Pos X
-by = uint8(bound(2)); % Pos Y
-bw = uint8(bound(3)); % Width X
-bh = uint8(bound(4)); % Height Y
+    Solidity = regions(k).Solidity(); %%Should be larger than 0.5
+    bound = regions(k).BoundingBox();
+    width = uint8(bound(3)); 
+    height = uint8(bound(4));
+    aspectRatio = width/height; %%Should be between 0.4 and 0.8
+    Orientation = regions(k).Orientation();
 
-regionMask(by:by+bh,bx:bx+bw) = Solidity > Threshold
-
+    if((Solidity > Threshold) && (aspectRatio > 0.4 ||aspectRatio < 0.8) ...
+            && (Orientation > -45 || Orientation < 45))
+        region = regions(k).PixelList();
+        outImage(region) = 0;
+    end
 end
 
-%outImage = regionMask;
-outImage = imclearborder(inImage); %%Remove objects touching the border.
 end
