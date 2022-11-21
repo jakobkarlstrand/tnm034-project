07b49761db122376983d2e76550314c1a3376147
Axel <axel.nie17@gmail.com>
2022-11-21 11:06:05 +0100

    Cleaned up code in skinprobability

--- skinmask.m
+++ skinmask.m
@@ -7,21 +7,20 @@ Cb = Cb*255;
 Cr = Cr*255;
 
 mask = zeros(size(currentImage, 1), size(currentImage, 2));
+
+%Get skin probability matrix
 matrix = skinprobabilitymatrix();
-imwrite(matrix, "skinprobability.png")
 %matrix_blurred = imgaussfilt(matrix,3);
 %imwrite(matrix_blurred, "skinprobability_blurred.png")
-
 matrix = matrix./max(matrix(:));
 
 
+%%Multiply currentImage with skin probability matrix
 for r = 1:size(mask, 1)    % for number of rows of the image
     for c = 1:size(mask, 2)    % for number of columns of the image
         mask(r,c) = matrix(round(Cb(r,c)), round(Cr(r,c)));
     end
 end
-
-figure;
 
 mask_threshold = mask > threshold;
 
--- skinprobabilitymatrix.m
+++ skinprobabilitymatrix.m
@@ -7,9 +7,9 @@ Im = cell(1,nfiles); %%Allocate space
 %%for each image
 for i = 1:nfiles
   filename=fullfile(folder,imagefiles(i).name); %%get filename of current image.
-  subplot(nfiles, nfiles, i); %%define location to plot the image in our subplot.
+  %%subplot(nfiles, nfiles, i); %%define location to plot the image in our subplot.
   Im{i}=im2double(imread(filename)); %%Read the imagefile, convert to double and store it.
-  imshow(Im{i}) %%Show the current image in a subplot.
+  %%imshow(Im{i}) %%Show the current image in a subplot.
 end
 
 
@@ -19,9 +19,6 @@ for i = 1:nfiles
   YCbCr = rgb2ycbcr(Im{i}); %%convert the first image in our folder to YCbCr.
   [Y, Cb, Cr] = imsplit(YCbCr); %%split the image into Y, Cb and Cr.'
   
-  if i == 1
-     disp(Y) 
-  end
   
   arrayCb = Cb';
   arrayCb = arrayCb(:)';
@@ -30,23 +27,18 @@ for i = 1:nfiles
   arrayY = Y';
   arrayY = arrayY(:)';
   
-  pixels_rejected = 0;
-  pixels = 0;
+
   
   for k = 1:numel(arrayCr)
       
       if arrayY(k) < 0.92 %%White pixels have value 0.9216
-        pixels = pixels + 1;
         prob_image(round(arrayCb(k)*255), round(arrayCr(k)*255)) = prob_image(round(arrayCb(k)*255), round(arrayCr(k)*255)) + 1;
-      else
-        pixels_rejected = pixels_rejected + 1;
+
       end
      
   end
-  disp(pixels_rejected)
-  disp(pixels)
+
   prob_image = prob_image./max(prob_image(:));
-  figure;
   
   
 end
